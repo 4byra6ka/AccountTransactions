1ff32c4b57206ee1f0e821d6f60815c4f522b1dd
GAU <gau@1jz.ru>
2023-02-22 21:54:24 +0300

    add docstr main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from utils.utils import load_operations, list_operations
 
 
 def main():
+    """
+    Запуск кода для виджета «Операции по счетам»
+    """
     url_operations = "https://skyengpublic.notion.site/signed/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure." \
                      "notion-static.com%2Fd22c7143-d55e-4f1d-aa98-e9b15e5e5efc%2Foperations.json?" \
                      "id=f11058ed-10ad-42ea-a13d-aad1945e5421&table=block&spaceId=0771f0bb-b4cb-4a14-bc05-" \
